2f752ed924712a91f283f57fb2b2b22ac1c97c7e
vlady <vlady3000hc@gmail.com>
2025-01-25 18:05:31 -0400

    INV-1 update the category crud

--- routes/api.php
+++ routes/api.php
@@ -12,18 +12,18 @@ use Illuminate\Support\Facades\Route;
 Route::post('/auth/login', [AuthController::class, 'login']);
 
 
-Route::middleware('auth:api')->group(function () {
+Route::middleware('auth:api')->group(function () {});
 
     Route::get('/me', [UserController::class, 'me']);
     Route::get('/users', [UserController::class, 'index']);
 //categories
-    Route::get('categories', [CategoryController::class, 'index']);
-    Route::post('categories', [CategoryController::class, 'store']);
-    Route::get('categories/{id}', [CategoryController::class, 'show']);
+    Route::get('categories', [CategoryController::class, 'getAll']);
+    Route::post('categories', [CategoryController::class, 'save']);
+    Route::get('categories/{id}', [CategoryController::class, 'get']);
     Route::delete('categories/{id}', [CategoryController::class, 'destroy']);
 //products
 Route::get('products', [ProductController::class, 'index']);
 Route::post('products', [ProductController::class, 'store']);
 Route::get('products/poleras', [ProductController::class, 'getProduct']);
 Route::post('/send-email', [ProductController::class, 'sendEmail']);
-});
+
